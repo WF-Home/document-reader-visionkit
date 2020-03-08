13bc3eff42357ae50c5101b539124a2514d8332f
wiljay.flores <wiljay.flores@loblaw.ca>
2020-03-08 00:57:23 -0500

    [WF-Home] created ios project

---
+++ document-reader-visionkit/document-reader-visionkit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  document-reader-visionkit
+//
+//  Created by Wiljay Flores on 2020-03-08.
+//  Copyright © 2020 Wiljay Flores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ document-reader-visionkit/document-reader-visionkitTests/document_reader_visionkitTests.swift
@@ -0,0 +1,34 @@
+//
+//  document_reader_visionkitTests.swift
+//  document-reader-visionkitTests
+//
+//  Created by Wiljay Flores on 2020-03-08.
+//  Copyright © 2020 Wiljay Flores. All rights reserved.
+//
+
+import XCTest
+@testable import document_reader_visionkit
+
+class document_reader_visionkitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
